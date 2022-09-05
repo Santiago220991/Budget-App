7e963a473e3cd369e102035a8026bb697651d224
Santiago <alexander.cardenas10@outlook.com>
2022-09-05 13:25:11 -0500

    Create tables

---
+++ db/migrate/20220905170546_rename_user_id_to_author_id.rb
@@ -0,0 +1,7 @@
+class RenameUserIdToAuthorId < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :expenses, :user_id, :author_id
+    rename_column :groups, :user_id, :author_id
+    
+  end
+end
