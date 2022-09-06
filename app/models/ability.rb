1349fa26c80c41d6688498fc6c8b9be36c53e02a
Santiago <alexander.cardenas10@outlook.com>
2022-09-06 17:51:50 -0500

    Check linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,7 @@ class Ability
     # Define abilities for the user here. For example:
     #
     return unless user.present?
+
     can :manage, Group, author_id: user.id
     can :manage, Expense, author_id: user.id
     can :manage, GroupExpense
